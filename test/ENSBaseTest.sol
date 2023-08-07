c72b37989fbd3ff80ce404cb1aeba7f7d2bc8def
zeroknots <zeroknots@protonmail.com>
2023-08-07 12:27:23 +0700

    working

--- test/ENSBaseTest.sol
+++ test/ENSBaseTest.sol
@@ -4,33 +4,55 @@ pragma solidity ^0.8.19;
 import "forge-std/Test.sol";
 
 import { ENSRegistry } from "ens-contracts/registry/ENSRegistry.sol";
+import { TestRegistrar } from "ens-contracts/registry/TestRegistrar.sol";
+import { BaseRegistrarImplementation } from
+    "ens-contracts/ethregistrar/BaseRegistrarImplementation.sol";
 import { ENSNamehash } from "../src/ENSNameHash.sol";
 
 /// @title ENSBaseTest
 /// @author zeroknots
 contract ENSBaseTest is Test {
     ENSRegistry ens;
+    BaseRegistrarImplementation registrar;
+
+    TestRegistrar rsRegistrar;
 
     address owner;
     address resolver;
 
-    function setUp() public {
-        ens = new ENSRegistry();
+    address controller;
+    bytes32 rhinestoneNode = ENSNamehash.namehash("rhinestone.eth");
+    address rhinestoneOwner;
 
+    function setUp() public {
         owner = makeAddr("owner");
         resolver = makeAddr("resolver");
-    }
+        controller = makeAddr("controller");
 
-    function testRegisterDomain() public {
-        ens.setRecord(0x0, owner, resolver, 3600);
+        ens = new ENSRegistry();
+        vm.warp(1641070800);
+        bytes32 baseNode = ENSNamehash.namehash("eth");
+        registrar = new BaseRegistrarImplementation(ens, baseNode);
+        registrar.addController(controller);
+        ens.setSubnodeOwner(0x0, keccak256("eth"), address(registrar));
+
+        rhinestoneOwner = makeAddr("registrant");
+        rsRegistrar = new TestRegistrar(ens, rhinestoneNode);
+
+        vm.prank(address(controller));
+        registrar.register({
+            id: uint256(keccak256("rhinestone")),
+            owner: address(rsRegistrar),
+            duration: 80 days
+        });
     }
 
     function testSubDomainRecord() public {
-        ens.setSubnodeRecord(0x0, keccak256("test"), owner, resolver, 3600);
+        bytes32 label = keccak256("module1");
+        address module1 = makeAddr("module1");
+        rsRegistrar.register(label, module1);
 
-        bytes32 namehash = ENSNamehash.namehash("test");
-        address _owner = ens.owner(namehash);
-
-        assertEq(_owner, owner);
+        address _module1 = ens.owner(ENSNamehash.namehash("module1.rhinestone.eth"));
+        assertEq(_module1, module1);
     }
 }
